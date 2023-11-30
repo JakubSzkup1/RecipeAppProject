90a0da3add7afa185176318deb008c2c43ca0a7e
JakubSzkup1 <jakub281@hotmail.com>
2023-11-30 22:58:14 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    <BrowserRouter>
 
+
+    
+      <div className="App">
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Container>
+          <Col>
+          <Image src="" roundedCircle />
+        </Col>
+            <Navbar.Brand href="#home">GoodEats</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Recipes</Nav.Link>{/*will be an api with recipes or mongodb database*/}
+                <Nav.Link href="#pricing">My Recipes</Nav.Link>
+                <Nav.Link href="#pricing">Add Recipes</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Something</Nav.Link>
+                
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </BrowserRouter>
+  );
+
+}
 export default App;
